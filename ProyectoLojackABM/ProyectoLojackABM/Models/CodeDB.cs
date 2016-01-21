626ec5c85510b5086a6007ef598eb750eb5595c1
GSystemsLucasMaspero <lmaspero@gsystems.com.ar>
2016-01-21 14:33:50 -0300

    Agregada clase de conexión a la base de datos (con sus meétodos Open, Close, DataInsert).

---
+++ ProyectoLojackABM/ProyectoLojackABM/Models/CodeDB.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace MVCPrueba.Models
+{
+    public class CodeDB
+    {
+        // Conexión de variables
+        protected SqlConnection connection;
+
+        // Abrir conexión
+        public bool Open(string Connection = "DefaultConnection")
+        {
+            connection = new SqlConnection(@WebConfigurationManager.ConnectionStrings[Connection].ToString());
+            try
+            {
+                bool b = true;
+                if (connection.State.ToString() != "Open")
+                {
+                    connection.Open();
+                }
+                return b;
+            }
+            catch (SqlException ex)
+            {
+                return false;
+            }
+        }
+
+        // Cerrar conexión
+        public bool Close()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                return false;
+            }
+        }
+
+        public int ToInt(object s)
+        {
+            try
+            {
+                return Int32.Parse(s.ToString());
+            } catch {
+                return 0;
+            }
+        }
+
+        public int DataInsert(string sql)
+        {
+            int LastID = 0;
+            string query = sql + ";SELECT @@Identity;";
+
+            try
+            {
+                if (connection.State.ToString() == "Open")
+                {
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.ExecuteNonQuery();
+                    LastID = this.ToInt(cmd.ExecuteScalar());
+                }
+                return this.ToInt(LastID);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+
+    }
+}
