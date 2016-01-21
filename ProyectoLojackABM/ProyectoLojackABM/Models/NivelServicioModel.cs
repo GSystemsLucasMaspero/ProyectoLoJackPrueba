227f9db8f013f945766f69577469d06be51a9732
GSystemsLucasMaspero <lmaspero@gsystems.com.ar>
2016-01-21 14:35:24 -0300

    Agregada la clase modelo de la tabla NivelServicio.

---
+++ ProyectoLojackABM/ProyectoLojackABM/Models/NivelServicioModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCPrueba.Models
+{
+    public class NivelServicioModel
+    {
+        
+        public DateTime? fechaBaja { get; set; }
+        public int? usuarioBaja { get; set; }
+        [Required]
+        public int idNivelServicio { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string descripcion { get; set; }
+        [Required]
+        public DateTime fechaAlta { get; set; }
+        [Required]
+        public int usuarioAlta { get; set; }
+    }
+
+}
